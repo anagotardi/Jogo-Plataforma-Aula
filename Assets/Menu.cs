c9630928a8444d13846bf1eb18e946f8d02d722d
Laboratório FIAP <labsfiap@fiap.com.br>
2024-08-05 13:39:48 -0300

    3/8

---
+++ Assets/Menu.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour
+
+{
+    private UIDocument document;
+    private Button botaoJogar;
+
+    private void Awake()
+    {
+        document = GetComponent<UIDocument>();
+        botaoJogar = document.rootVisualElement.Q<Button>("btn_Jogar");
+        botaoJogar.RegisterCallback<ClickEvent>(OnPlayGame);
+    }
+
+    void OnPlayGame(ClickEvent evt) 
+    {
+        SceneManager.LoadScene("Main");
+    }
+
+}
+
+
